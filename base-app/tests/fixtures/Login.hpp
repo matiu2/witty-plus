74fbfccf9e47051d8a1f2f78b96d2b5a80973a55
Matthew Sherborne <msherborne@gmail.com>
2011-09-11 19:14:43 +1000

    Moved the login fixture to it's own file so I can reuse it for the user manager tests

---
+++ base-app/tests/fixtures/Login.hpp
@@ -0,0 +1,63 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Login.hpp
+ *
+ *    Description:  Login Fixture
+ *
+ *        Version:  1.0
+ *        Created:  09/11/2011 18:57:44
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef TESTS_FIXTURES_LOGIN_HPP
+#define TESTS_FIXTURES_LOGIN_HPP
+
+#include "App.hpp"
+#include "../helpers.hpp"
+
+
+namespace my_app {
+namespace unittests {
+namespace fixtures {
+
+namespace h = helpers;
+/// Our special login fixture for testing the login window
+struct LoginFixture : public AppFixture {
+    /// Clicks the login link
+    LoginWindow* clickLogin() {
+        h::click(main->_loginLink);
+        LoginWindow* result = main->resolve<LoginWindow*>("content");
+        BOOST_REQUIRE_MESSAGE( result, "Looks like the login window didn't appear" );
+        return result;
+    }
+    /// Makes sure we *are* logged in
+    void checkLoggedIn() {
+        // Make sure the login link died
+        BOOST_CHECK_MESSAGE( main->_loginLink == 0, "Login link should have disappeared. Maybe admin+admin is not in DB ?" );
+        // Make sure the control panel appears
+        AdminIndex* cp = main->resolve<AdminIndex*>("controls");
+        BOOST_CHECK_MESSAGE( cp, "Looks like the control panel didn't appear" );
+        // Make sure the logout link is there
+        BOOST_CHECK_MESSAGE( cp->resolve<wittyPlus::InternalLink*>("link-logout"),
+                             "Looks like the logout link didn't appear" );
+    }
+    /// Makes sure we're *not* logged in
+    void checkLoggedOut(const std::string& path="/") {
+        if (!path.empty())
+            BOOST_CHECK_EQUAL( app.internalPath(), path );
+        BOOST_CHECK_MESSAGE( main->_loginLink, "The login ling should still be there" );
+    }
+};
+
+} // namespace fixtures
+} // namespace unittests 
+} // namespace my_app
+
+#endif // TESTS_FIXTURES_LOGIN_HPP
--- base-app/tests/test_login.cpp
+++ base-app/tests/test_login.cpp
@@ -25,44 +25,12 @@
 #include "../LoginWindow.hpp"
 #include "../lib/ButtonBar.hpp"
 #include "../AdminIndex.hpp"
-#include <Wt/WPushButton>
-#include "fixtures/App.hpp"
-#include "helpers.hpp"
+#include "fixtures/Login.hpp"
 
 namespace my_app {
 namespace unittests {
 
 namespace h = helpers;
-
-/// Our special login fixture for testing the login window
-namespace fixtures {
-struct LoginFixture : public AppFixture {
-    /// Clicks the login link
-    LoginWindow* clickLogin() {
-        h::click(main->_loginLink);
-        LoginWindow* result = main->resolve<LoginWindow*>("content");
-        BOOST_REQUIRE_MESSAGE( result, "Looks like the login window didn't appear" );
-        return result;
-    }
-    /// Makes sure we *are* logged in
-    void checkLoggedIn() {
-        // Make sure the login link died
-        BOOST_CHECK_MESSAGE( main->_loginLink == 0, "Login link should have disappeared. Maybe admin+admin is not in DB ?" );
-        // Make sure the control panel appears
-        AdminIndex* cp = main->resolve<AdminIndex*>("controls");
-        BOOST_CHECK_MESSAGE( cp, "Looks like the control panel didn't appear" );
-        // Make sure the logout link is there
-        BOOST_CHECK_MESSAGE( cp->resolve<wittyPlus::InternalLink*>("link-logout"),
-                             "Looks like the logout link didn't appear" );
-    }
-    /// Makes sure we're *not* logged in
-    void checkLoggedOut(const std::string& path="/") {
-        if (!path.empty())
-            BOOST_CHECK_EQUAL( app.internalPath(), path );
-        BOOST_CHECK_MESSAGE( main->_loginLink, "The login ling should still be there" );
-    }
-};
-} // namespace fixtures
 
 // /////// Tests start here //////////////////////////////////////////////
 
