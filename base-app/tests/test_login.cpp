e4a1f909e573197dfec4b6c6813a0528009634a5
Matthew Sherborne <msherborne@gmail.com>
2011-09-11 17:26:09 +1000

    Tests login cancelled  and login failed

--- base-app/tests/test_login.cpp
+++ base-app/tests/test_login.cpp
@@ -48,8 +48,9 @@ BOOST_AUTO_TEST_CASE( login_escape_test ) {
     LoginWindow* login = clickLogin(main);
     // Hit escape
     h::keyPress(login->_passwordEdit, 27); // Hit Escape
-    // Should now be back at the original location
+    // Make sure we didn't log in
     BOOST_CHECK_EQUAL( app.internalPath(), "/" );
+    BOOST_CHECK_MESSAGE( main->_loginLink, "The login ling should still be there" );
 }
 
 BOOST_AUTO_TEST_CASE( login_enter_test ) {
@@ -69,13 +70,39 @@ BOOST_AUTO_TEST_CASE( login_ok_test ) {
     // Fill in the form
     login->_usernameEdit->setText("admin");
     login->_passwordEdit->setText("admin");
-    // Click the OK button
+    // Click the Login button
     h::click(login->_btnBar, "Login");
     app.processEvents();
     // Check that we're logged in
     BOOST_CHECK_MESSAGE( main->_loginLink == 0, "Login link should have disappeared. Maybe admin+admin is not in DB ?" );
     AdminIndex* cp = main->resolve<AdminIndex*>("controls");
     BOOST_REQUIRE_MESSAGE( cp, "Looks like the control panel didn't appear" );
+}
+
+BOOST_AUTO_TEST_CASE( login_cancel_test ) {
+    LoginWindow* login = clickLogin(main);
+    // Fill in the form
+    login->_usernameEdit->setText("admin");
+    login->_passwordEdit->setText("admin");
+    // Click the Cancel button
+    h::click(login->_btnBar, "Cancel");
+    app.processEvents();
+    // Make sure we didn't log in
+    BOOST_CHECK_EQUAL( app.internalPath(), "/" );
+    BOOST_CHECK_MESSAGE( main->_loginLink, "The login ling should still be there" );
+}
+
+BOOST_AUTO_TEST_CASE( login_fail_test ) {
+    LoginWindow* login = clickLogin(main);
+    // Fill in the form
+    login->_usernameEdit->setText("NOT a User");
+    login->_passwordEdit->setText("Bad Pass");
+    // Click the Login button
+    h::click(login->_btnBar, "Login");
+    app.processEvents();
+    // Make sure we didn't log in
+    BOOST_CHECK_EQUAL( app.internalPath(), "/" );
+    BOOST_CHECK_MESSAGE( main->_loginLink, "The login ling should still be there" );
 }
 
 BOOST_AUTO_TEST_SUITE_END() // login_suite
