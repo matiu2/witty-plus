9d1f8f0827642c37d37bc030c0fb3e08e1ce0b30
Matthew Sherborne <msherborne@gmail.com>
2011-09-13 22:05:56 +1000

    Added more to the new_user test

--- base-app/tests/test_usermanager.cpp
+++ base-app/tests/test_usermanager.cpp
@@ -20,8 +20,10 @@
 
 #include "helpers.hpp"
 #include "fixtures/UserManager.hpp"
+#include <Wt/WSelectionBox>
 #include <boost/test/unit_test.hpp>
 #include <boost/test/test_tools.hpp>
+#include <iostream>
 
 namespace my_app {
 namespace unittests {
@@ -31,13 +33,18 @@ namespace h = helpers;
 BOOST_FIXTURE_TEST_SUITE( user_manager_suite, fixtures::UserManagerFixture );
 
 BOOST_AUTO_TEST_CASE( new_user ) {
+    // Get this list of users
     UserList* ul = userList();
     BOOST_REQUIRE_MESSAGE( ul->isVisible(), "We expected the userlist to be visible" );
+    // Make sure only one uer in the list at first
+    BOOST_REQUIRE_EQUAL( ul->lstUsers->count(), 1 );
+    // Click the new user button
     h::click(ul->btnNew);
     UserEdit* ue = userEdit();
     BOOST_CHECK_MESSAGE( ue->isVisible(), "Click new should show the UserEdit screen" );
     // Now fill in the new user details
-    ue->edtName->setText("Test User 1");
+    const string testUsername = "Test User 1";
+    ue->edtName->setText(testUsername);
     h::keyPress(ue->edtName, 13);
     BOOST_CHECK_MESSAGE(ue->edtPass1->hasFocus(),
         "Clicking enter after typing your name should give focus to the 1st password field");
@@ -48,8 +55,10 @@ BOOST_AUTO_TEST_CASE( new_user ) {
     ul = userList();
     BOOST_REQUIRE_MESSAGE( ul->isVisible(),
         "We expected it to go back to the user list after hitting enter when editing a user");
-
-
+    // Check our new user is in the list
+    BOOST_CHECK_GT( ul->lstUsers->count(), 1); // Need more than one user
+    int index = ul->lstUsers->findText(testUsername, Wt::MatchStringExactly);
+    BOOST_CHECK_MESSAGE( index >= 0, "Couldn't find our user in the list" );
 }
 
 } // namespace unittests
