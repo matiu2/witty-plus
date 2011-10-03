7615423d649f14aba69470bced59d00cc21944f1
Matthew Sherborne <msherborne@gmail.com>
2011-10-03 11:47:16 +1000

    Seems git didn't pick up some doco files

---
+++ reference/navtree.js
@@ -0,0 +1,388 @@
+var NAVTREE =
+[
+  [ "wittyPlus Base App", "index.html", [
+    [ "Class List", "annotated.html", [
+      [ "my_app::AdminIndex", "classmy__app_1_1_admin_index.html", null ],
+      [ "my_app::App", "classmy__app_1_1_app.html", null ],
+      [ "my_app::unittests::fixtures::AppFixture", "structmy__app_1_1unittests_1_1fixtures_1_1_app_fixture.html", null ],
+      [ "wittyPlus::BaseApp< User >", "classwitty_plus_1_1_base_app.html", null ],
+      [ "wittyPlus::BaseMatchValidator", "classwitty_plus_1_1_base_match_validator.html", null ],
+      [ "wittyPlus::BaseSessionHandle", "classwitty_plus_1_1_base_session_handle.html", null ],
+      [ "wittyPlus::ButtonBar", "classwitty_plus_1_1_button_bar.html", null ],
+      [ "wittyPlus::DBNoDupValidator< ModelClass >", "classwitty_plus_1_1_d_b_no_dup_validator.html", null ],
+      [ "wittyPlus::FieldRow", "structwitty_plus_1_1_field_row.html", null ],
+      [ "wittyPlus::HasTextConcept< WidgetType >", "structwitty_plus_1_1_has_text_concept.html", null ],
+      [ "wittyPlus::InternalLink", "classwitty_plus_1_1_internal_link.html", null ],
+      [ "my_app::unittests::fixtures::LoginFixture", "structmy__app_1_1unittests_1_1fixtures_1_1_login_fixture.html", null ],
+      [ "my_app::LoginWindow", "classmy__app_1_1_login_window.html", null ],
+      [ "my_app::MainWindow", "classmy__app_1_1_main_window.html", null ],
+      [ "wittyPlus::MatchValidator< WidgetType >", "structwitty_plus_1_1_match_validator.html", null ],
+      [ "wittyPlus::MatchValidator< WComboBox >", "structwitty_plus_1_1_match_validator_3_01_w_combo_box_01_4.html", null ],
+      [ "wittyPlus::MemorySessionStore", "classwitty_plus_1_1_memory_session_store.html", null ],
+      [ "wittyPlus::MoreAwesomeTemplate", "classwitty_plus_1_1_more_awesome_template.html", null ],
+      [ "wittyPlus::ServerSideValidationResult", "structwitty_plus_1_1_server_side_validation_result.html", null ],
+      [ "wittyPlus::ServerSideValidator", "classwitty_plus_1_1_server_side_validator.html", null ],
+      [ "wittyPlus::Session", "classwitty_plus_1_1_session.html", null ],
+      [ "wittyPlus::SessionHandle< SessionStore, User >", "classwitty_plus_1_1_session_handle.html", null ],
+      [ "wittyPlus::SessionManager", "classwitty_plus_1_1_session_manager.html", null ],
+      [ "my_app::URL2Action", "classmy__app_1_1_u_r_l2_action.html", null ],
+      [ "wittyPlus::URLs", "classwitty_plus_1_1_u_r_ls.html", null ],
+      [ "my_app::model::User", "classmy__app_1_1model_1_1_user.html", null ],
+      [ "my_app::UserEdit", "classmy__app_1_1_user_edit.html", null ],
+      [ "my_app::UserList", "classmy__app_1_1_user_list.html", null ],
+      [ "my_app::UserManager", "classmy__app_1_1_user_manager.html", null ],
+      [ "my_app::unittests::fixtures::UserManagerFixture", "structmy__app_1_1unittests_1_1fixtures_1_1_user_manager_fixture.html", null ],
+      [ "WT_TestFixture", "struct_w_t___test_fixture.html", null ]
+    ] ],
+    [ "Class Index", "classes.html", null ],
+    [ "Class Hierarchy", "hierarchy.html", [
+      [ "my_app::unittests::fixtures::AppFixture", "structmy__app_1_1unittests_1_1fixtures_1_1_app_fixture.html", [
+        [ "my_app::unittests::fixtures::LoginFixture", "structmy__app_1_1unittests_1_1fixtures_1_1_login_fixture.html", [
+          [ "my_app::unittests::fixtures::UserManagerFixture", "structmy__app_1_1unittests_1_1fixtures_1_1_user_manager_fixture.html", null ]
+        ] ]
+      ] ],
+      [ "wittyPlus::BaseApp< User >", "classwitty_plus_1_1_base_app.html", [
+        [ "my_app::App", "classmy__app_1_1_app.html", null ]
+      ] ],
+      [ "wittyPlus::BaseMatchValidator", "classwitty_plus_1_1_base_match_validator.html", [
+        [ "wittyPlus::MatchValidator< WidgetType >", "structwitty_plus_1_1_match_validator.html", null ],
+        [ "wittyPlus::MatchValidator< WComboBox >", "structwitty_plus_1_1_match_validator_3_01_w_combo_box_01_4.html", null ]
+      ] ],
+      [ "wittyPlus::BaseSessionHandle", "classwitty_plus_1_1_base_session_handle.html", [
+        [ "wittyPlus::SessionHandle< SessionStore, User >", "classwitty_plus_1_1_session_handle.html", null ]
+      ] ],
+      [ "wittyPlus::FieldRow", "structwitty_plus_1_1_field_row.html", null ],
+      [ "wittyPlus::HasTextConcept< WidgetType >", "structwitty_plus_1_1_has_text_concept.html", null ],
+      [ "wittyPlus::InternalLink", "classwitty_plus_1_1_internal_link.html", null ],
+      [ "wittyPlus::MemorySessionStore", "classwitty_plus_1_1_memory_session_store.html", null ],
+      [ "wittyPlus::MoreAwesomeTemplate", "classwitty_plus_1_1_more_awesome_template.html", [
+        [ "my_app::AdminIndex", "classmy__app_1_1_admin_index.html", null ],
+        [ "my_app::LoginWindow", "classmy__app_1_1_login_window.html", null ],
+        [ "my_app::MainWindow", "classmy__app_1_1_main_window.html", null ],
+        [ "my_app::UserEdit", "classmy__app_1_1_user_edit.html", null ],
+        [ "my_app::UserList", "classmy__app_1_1_user_list.html", null ],
+        [ "wittyPlus::ButtonBar", "classwitty_plus_1_1_button_bar.html", null ]
+      ] ],
+      [ "wittyPlus::ServerSideValidationResult", "structwitty_plus_1_1_server_side_validation_result.html", null ],
+      [ "wittyPlus::ServerSideValidator", "classwitty_plus_1_1_server_side_validator.html", [
+        [ "wittyPlus::DBNoDupValidator< ModelClass >", "classwitty_plus_1_1_d_b_no_dup_validator.html", null ]
+      ] ],
+      [ "wittyPlus::Session", "classwitty_plus_1_1_session.html", null ],
+      [ "wittyPlus::SessionManager", "classwitty_plus_1_1_session_manager.html", null ],
+      [ "my_app::URL2Action", "classmy__app_1_1_u_r_l2_action.html", null ],
+      [ "wittyPlus::URLs", "classwitty_plus_1_1_u_r_ls.html", null ],
+      [ "my_app::model::User", "classmy__app_1_1model_1_1_user.html", null ],
+      [ "my_app::UserManager", "classmy__app_1_1_user_manager.html", null ],
+      [ "WT_TestFixture", "struct_w_t___test_fixture.html", null ]
+    ] ],
+    [ "Class Members", "functions.html", null ],
+    [ "Namespace List", "namespaces.html", [
+      [ "my_app", "namespacemy__app.html", null ],
+      [ "my_app::model", "namespacemy__app_1_1model.html", null ],
+      [ "my_app::unittests", "namespacemy__app_1_1unittests.html", null ],
+      [ "my_app::unittests::fixtures", "namespacemy__app_1_1unittests_1_1fixtures.html", null ],
+      [ "my_app::unittests::helpers", "namespacemy__app_1_1unittests_1_1helpers.html", null ],
+      [ "my_app::urls", "namespacemy__app_1_1urls.html", null ],
+      [ "wittyPlus", "namespacewitty_plus.html", null ],
+      [ "Wt", "namespace_wt.html", null ]
+    ] ],
+    [ "Namespace Members", "namespacemembers.html", null ],
+    [ "File List", "files.html", [
+      [ "base-app/AdminIndex.hpp", "_admin_index_8hpp.html", null ],
+      [ "base-app/App.cpp", "_app_8cpp.html", null ],
+      [ "base-app/App.hpp", "_app_8hpp.html", null ],
+      [ "base-app/common.hpp", "common_8hpp.html", null ],
+      [ "base-app/db.hpp", "db_8hpp.html", null ],
+      [ "base-app/LoginWindow.cpp", "_login_window_8cpp.html", null ],
+      [ "base-app/LoginWindow.hpp", "_login_window_8hpp.html", null ],
+      [ "base-app/main.cpp", "main_8cpp.html", null ],
+      [ "base-app/MainWindow.cpp", "_main_window_8cpp.html", null ],
+      [ "base-app/MainWindow.hpp", "_main_window_8hpp.html", null ],
+      [ "base-app/setupDB.cpp", "setup_d_b_8cpp.html", null ],
+      [ "base-app/url2action.hpp", "url2action_8hpp.html", null ],
+      [ "base-app/urls.cpp", "urls_8cpp.html", null ],
+      [ "base-app/urls.hpp", "urls_8hpp.html", null ],
+      [ "base-app/UserEdit.cpp", "_user_edit_8cpp.html", null ],
+      [ "base-app/UserEdit.hpp", "_user_edit_8hpp.html", null ],
+      [ "base-app/UserList.cpp", "_user_list_8cpp.html", null ],
+      [ "base-app/UserList.hpp", "_user_list_8hpp.html", null ],
+      [ "base-app/UserManager.hpp", "_user_manager_8hpp.html", null ],
+      [ "base-app/build/CMakeFiles/CompilerIdC/CMakeCCompilerId.c", "base-app_2build_2_c_make_files_2_compiler_id_c_2_c_make_c_compiler_id_8c.html", null ],
+      [ "base-app/build/CMakeFiles/CompilerIdCXX/CMakeCXXCompilerId.cpp", "base-app_2build_2_c_make_files_2_compiler_id_c_x_x_2_c_make_c_x_x_compiler_id_8cpp.html", null ],
+      [ "base-app/lib/BaseApp.hpp", "_base_app_8hpp.html", null ],
+      [ "base-app/lib/BaseSessionHandle.cpp", "_base_session_handle_8cpp.html", null ],
+      [ "base-app/lib/BaseSessionHandle.hpp", "_base_session_handle_8hpp.html", null ],
+      [ "base-app/lib/ButtonBar.hpp", "_button_bar_8hpp.html", null ],
+      [ "base-app/lib/DBNoDupValidator.hpp", "_d_b_no_dup_validator_8hpp.html", null ],
+      [ "base-app/lib/InternalLink.hpp", "_internal_link_8hpp.html", null ],
+      [ "base-app/lib/MatchValidator.hpp", "_match_validator_8hpp.html", null ],
+      [ "base-app/lib/MemorySessionStore.cpp", "_memory_session_store_8cpp.html", null ],
+      [ "base-app/lib/MemorySessionStore.hpp", "_memory_session_store_8hpp.html", null ],
+      [ "base-app/lib/MoreAwesomeTemplate.hpp", "_more_awesome_template_8hpp.html", null ],
+      [ "base-app/lib/ServerSideValidator.hpp", "_server_side_validator_8hpp.html", null ],
+      [ "base-app/lib/SessionHandle.hpp", "_session_handle_8hpp.html", null ],
+      [ "base-app/lib/SessionManager.hpp", "_session_manager_8hpp.html", null ],
+      [ "base-app/lib/sha.hpp", "sha_8hpp.html", null ],
+      [ "base-app/lib/URLs.hpp", "_u_r_ls_8hpp.html", null ],
+      [ "base-app/model/User.hpp", "_user_8hpp.html", null ],
+      [ "base-app/tests/helpers.hpp", "helpers_8hpp.html", null ],
+      [ "base-app/tests/main.cpp", "tests_2main_8cpp.html", null ],
+      [ "base-app/tests/test_login.cpp", "test__login_8cpp.html", null ],
+      [ "base-app/tests/test_usermanager.cpp", "test__usermanager_8cpp.html", null ],
+      [ "base-app/tests/test_usermanager.hpp", "test__usermanager_8hpp.html", null ],
+      [ "base-app/tests/wt_test_base.hpp", "wt__test__base_8hpp.html", null ],
+      [ "base-app/tests/fixtures/App.hpp", "tests_2fixtures_2_app_8hpp.html", null ],
+      [ "base-app/tests/fixtures/Login.hpp", "_login_8hpp.html", null ],
+      [ "base-app/tests/fixtures/UserManager.hpp", "tests_2fixtures_2_user_manager_8hpp.html", null ],
+      [ "qtcreator-build/CMakeFiles/CompilerIdC/CMakeCCompilerId.c", "qtcreator-build_2_c_make_files_2_compiler_id_c_2_c_make_c_compiler_id_8c.html", null ],
+      [ "qtcreator-build/CMakeFiles/CompilerIdCXX/CMakeCXXCompilerId.cpp", "qtcreator-build_2_c_make_files_2_compiler_id_c_x_x_2_c_make_c_x_x_compiler_id_8cpp.html", null ]
+    ] ],
+    [ "File Members", "globals.html", null ]
+  ] ]
+];
+
+function createIndent(o,domNode,node,level)
+{
+  if (node.parentNode && node.parentNode.parentNode)
+  {
+    createIndent(o,domNode,node.parentNode,level+1);
+  }
+  var imgNode = document.createElement("img");
+  if (level==0 && node.childrenData)
+  {
+    node.plus_img = imgNode;
+    node.expandToggle = document.createElement("a");
+    node.expandToggle.href = "javascript:void(0)";
+    node.expandToggle.onclick = function() 
+    {
+      if (node.expanded) 
+      {
+        $(node.getChildrenUL()).slideUp("fast");
+        if (node.isLast)
+        {
+          node.plus_img.src = node.relpath+"ftv2plastnode.png";
+        }
+        else
+        {
+          node.plus_img.src = node.relpath+"ftv2pnode.png";
+        }
+        node.expanded = false;
+      } 
+      else 
+      {
+        expandNode(o, node, false);
+      }
+    }
+    node.expandToggle.appendChild(imgNode);
+    domNode.appendChild(node.expandToggle);
+  }
+  else
+  {
+    domNode.appendChild(imgNode);
+  }
+  if (level==0)
+  {
+    if (node.isLast)
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2plastnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2lastnode.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+    else
+    {
+      if (node.childrenData)
+      {
+        imgNode.src = node.relpath+"ftv2pnode.png";
+      }
+      else
+      {
+        imgNode.src = node.relpath+"ftv2node.png";
+        domNode.appendChild(imgNode);
+      }
+    }
+  }
+  else
+  {
+    if (node.isLast)
+    {
+      imgNode.src = node.relpath+"ftv2blank.png";
+    }
+    else
+    {
+      imgNode.src = node.relpath+"ftv2vertline.png";
+    }
+  }
+  imgNode.border = "0";
+}
+
+function newNode(o, po, text, link, childrenData, lastNode)
+{
+  var node = new Object();
+  node.children = Array();
+  node.childrenData = childrenData;
+  node.depth = po.depth + 1;
+  node.relpath = po.relpath;
+  node.isLast = lastNode;
+
+  node.li = document.createElement("li");
+  po.getChildrenUL().appendChild(node.li);
+  node.parentNode = po;
+
+  node.itemDiv = document.createElement("div");
+  node.itemDiv.className = "item";
+
+  node.labelSpan = document.createElement("span");
+  node.labelSpan.className = "label";
+
+  createIndent(o,node.itemDiv,node,0);
+  node.itemDiv.appendChild(node.labelSpan);
+  node.li.appendChild(node.itemDiv);
+
+  var a = document.createElement("a");
+  node.labelSpan.appendChild(a);
+  node.label = document.createTextNode(text);
+  a.appendChild(node.label);
+  if (link) 
+  {
+    a.href = node.relpath+link;
+  } 
+  else 
+  {
+    if (childrenData != null) 
+    {
+      a.className = "nolink";
+      a.href = "javascript:void(0)";
+      a.onclick = node.expandToggle.onclick;
+      node.expanded = false;
+    }
+  }
+
+  node.childrenUL = null;
+  node.getChildrenUL = function() 
+  {
+    if (!node.childrenUL) 
+    {
+      node.childrenUL = document.createElement("ul");
+      node.childrenUL.className = "children_ul";
+      node.childrenUL.style.display = "none";
+      node.li.appendChild(node.childrenUL);
+    }
+    return node.childrenUL;
+  };
+
+  return node;
+}
+
+function showRoot()
+{
+  var headerHeight = $("#top").height();
+  var footerHeight = $("#nav-path").height();
+  var windowHeight = $(window).height() - headerHeight - footerHeight;
+  navtree.scrollTo('#selected',0,{offset:-windowHeight/2});
+}
+
+function expandNode(o, node, imm)
+{
+  if (node.childrenData && !node.expanded) 
+  {
+    if (!node.childrenVisited) 
+    {
+      getNode(o, node);
+    }
+    if (imm)
+    {
+      $(node.getChildrenUL()).show();
+    } 
+    else 
+    {
+      $(node.getChildrenUL()).slideDown("fast",showRoot);
+    }
+    if (node.isLast)
+    {
+      node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+    }
+    else
+    {
+      node.plus_img.src = node.relpath+"ftv2mnode.png";
+    }
+    node.expanded = true;
+  }
+}
+
+function getNode(o, po)
+{
+  po.childrenVisited = true;
+  var l = po.childrenData.length-1;
+  for (var i in po.childrenData) 
+  {
+    var nodeData = po.childrenData[i];
+    po.children[i] = newNode(o, po, nodeData[0], nodeData[1], nodeData[2],
+        i==l);
+  }
+}
+
+function findNavTreePage(url, data)
+{
+  var nodes = data;
+  var result = null;
+  for (var i in nodes) 
+  {
+    var d = nodes[i];
+    if (d[1] == url) 
+    {
+      return new Array(i);
+    }
+    else if (d[2] != null) // array of children
+    {
+      result = findNavTreePage(url, d[2]);
+      if (result != null) 
+      {
+        return (new Array(i).concat(result));
+      }
+    }
+  }
+  return null;
+}
+
+function initNavTree(toroot,relpath)
+{
+  var o = new Object();
+  o.toroot = toroot;
+  o.node = new Object();
+  o.node.li = document.getElementById("nav-tree-contents");
+  o.node.childrenData = NAVTREE;
+  o.node.children = new Array();
+  o.node.childrenUL = document.createElement("ul");
+  o.node.getChildrenUL = function() { return o.node.childrenUL; };
+  o.node.li.appendChild(o.node.childrenUL);
+  o.node.depth = 0;
+  o.node.relpath = relpath;
+
+  getNode(o, o.node);
+
+  o.breadcrumbs = findNavTreePage(toroot, NAVTREE);
+  if (o.breadcrumbs == null)
+  {
+    o.breadcrumbs = findNavTreePage("index.html",NAVTREE);
+  }
+  if (o.breadcrumbs != null && o.breadcrumbs.length>0)
+  {
+    var p = o.node;
+    for (var i in o.breadcrumbs) 
+    {
+      var j = o.breadcrumbs[i];
+      p = p.children[j];
+      expandNode(o,p,true);
+    }
+    p.itemDiv.className = p.itemDiv.className + " selected";
+    p.itemDiv.id = "selected";
+    $(window).load(showRoot);
+  }
+}
+
